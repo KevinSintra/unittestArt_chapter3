9c7ba63f10b44aadcb0323826ad10a79d038a2aa
KevinPro <kevin79713@gmail.com>
2021-10-13 14:20:51 +0800

    重構: 考量到測試, 所以新增一個介面

--- LogAnalyzer.py
+++ LogAnalyzer.py
@@ -1,10 +1,20 @@
+from abc import ABCMeta, abstractmethod  # 提供抽象行為
+
+
+class IExtensionManager(metaclass=ABCMeta):  # 介面
+    '''為了讓測試能進行, 所以重構程式時增加該介面'''
+    @abstractmethod
+    def isValid(self, fileName: str) -> bool:
+        pass
+
+
 class LogAnalyzer:
     def isValidLogFileName(self, fileName):
         mgr = FileExtensionManager()
-        return mgr.IsValid(fileName)
+        return mgr.isValid(fileName)
 
 
-class FileExtensionManager:
-    def IsValid(self, fileName):
+class FileExtensionManager(IExtensionManager):  # 繼承
+    def isValid(self, fileName: str) -> bool:
         # 這裡是要讀取外部檔案, 先不進行實作
         return True
